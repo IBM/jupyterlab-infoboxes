19e4c67852cf1040e6642e637c660c5f5e7c3a4f
James Reeve <james.reeve@ibm.com>
2019-03-09 16:12:23 -0500

    random placement of infoboxes

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,22 @@ import '../style/index.css';
 
 const converter = new showdown.Converter();
 
+/**
+ * Checks if the html element being passed is a markdown cell containing a title
+ */
+function cellIsATitle(cell: Element) {              
+  return cell.children[1].children[1].children.length >= 3 && cell.children[1].children[1].children[2].innerHTML.startsWith("<h")
+}
+
+/**
+ * Modifies the watson studio cocl.us link to include the location of the link
+ * Just exists to test the best link placement -- this can be removed after 
+ * the best placement has been discovered
+ */
+function updateLinkWithLocation(infoBox: Node, location: String) {
+  (<HTMLElement> infoBox).innerHTML = (<HTMLElement> infoBox).innerHTML.replace("https://cocl.us/ibm_watson_studio_infobox", "https://cocl.us/ibm_watson_studio_infobox_"+location)
+}
+
 function createInfoBox(): Promise<Node> {
   return new Promise((resolve, reject) => {
 
@@ -32,6 +48,10 @@ function createInfoBox(): Promise<Node> {
                 var infoBox = document.createElement('div');
                 infoBox.className = "jupyterlab-infoboxes p-Widget jp-Cell jp-CodeCell jp-RenderedHTMLCommon jp-RenderedMarkdown";
                 infoBox.innerHTML = converter.makeHtml(markdown)
+                infoBox.style.backgroundColor = "#edf4ff";
+                infoBox.style.borderColor = "#0062ff";
+                infoBox.style.borderWidth = "2px";
+                infoBox.style.borderStyle = "solid";
                 resolve(infoBox);
               }, 300);
             })
@@ -78,7 +98,40 @@ const extension: JupyterLabPlugin<void> = {
           notebook.node.classList.add("has-jupyterlab-infoboxes");
           createInfoBox().then((infoBox) => {
             if (isEmptyNotebook(notebook)) {return;};
-            notebook.node.childNodes[1].appendChild(infoBox);
+
+            // randomly decide on a location, and edit the url accordingly
+            
+            var r = Math.floor(Math.random()*4);
+            switch (r) {
+              case 0:
+                // option 1: place at the beginning of notebook
+                updateLinkWithLocation(infoBox, "start");
+                notebook.node.childNodes[1].insertBefore(infoBox, notebook.node.childNodes[1].firstChild)
+                break;
+              case 1:
+                // option 2: place at the end of notebook
+                updateLinkWithLocation(infoBox, "end");
+                notebook.node.childNodes[1].appendChild(infoBox);
+                break;
+              case 2:
+                // option 3: place near the 1/4 point
+                updateLinkWithLocation(infoBox, "quarter");
+                var insertionPoint = Math.floor((<Element> notebook.node.childNodes[1]).children.length/4);
+                while (!(cellIsATitle((<Element> notebook.node.childNodes[1]).children[insertionPoint]))) {
+                  insertionPoint++;
+                }
+                notebook.node.childNodes[1].insertBefore(infoBox, (<Element> notebook.node.childNodes[1]).children[insertionPoint])
+                break;
+              case 3:
+                // option 4: place near halfway point
+                updateLinkWithLocation(infoBox, "half");
+                var insertionPoint = Math.floor((<Element> notebook.node.childNodes[1]).children.length/2);
+                while (!(cellIsATitle((<Element> notebook.node.childNodes[1]).children[insertionPoint]))) {
+                  insertionPoint++;
+                }
+                notebook.node.childNodes[1].insertBefore(infoBox, (<Element> notebook.node.childNodes[1]).children[insertionPoint])
+                break;
+            }
           })
         }
       })
